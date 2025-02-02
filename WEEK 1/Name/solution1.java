6a1b510ba88c122d43a0d7ae892f8f966c36164b
Hyochang098 <wlsgyckd7@naver.com>
2025-02-02 20:08:42 +0900

    기본문제풀이
    11279 최대 힙
    28279 덱 2

--- WEEK 1/Name/solution1.java
+++ WEEK 1/Name/solution1.java
@@ -1,13 +1,77 @@
-// 문제: 백준 00000번 문제제목
-// 링크: https://www.acmicpc.net/problem/00000
-// 시간: 000ms
+// 문제: 백준 28279 덱 2
+// 링크: https://www.acmicpc.net/problem/28279
+// 시간: 2232ms
 
-public class Main {
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class solution1{
 	public static void main(String[] args) {
-
-        // 이곳에 풀이를 작성합니다.
-        // 다른 사람을 위해 주석을 꼭 달아주세요.
-        // 실패했다면 실패한 이유도 함께 적어주세요.
-
-    }
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();//시간초과 문제로 한번에 출력하는 stringbulider사용
+		int n = sc.nextInt();
+		sc.nextLine();//개행처리
+		
+		Deque<Integer> deque = new LinkedList<Integer>();//deque 생성
+		for(int i=0;i<n;i++) {
+			String str = sc.nextLine();
+			String[] arr = str.split(" ");//공백으로 분리
+			int a = Integer.parseInt(arr[0]);//명령어
+			switch (a) {
+			case 1:
+				int num = Integer.parseInt(arr[1]);//숫자변환
+				deque.addFirst(num);//앞에 추가
+				break;
+			case 2:
+				int b_num = Integer.parseInt(arr[1]);
+				deque.addLast(b_num);//뒤에추가
+				break;
+			case 3:
+				if(deque.isEmpty()) {//비어있는지확인
+					sb.append(-1).append("\n");//입력하고 이후 ENTER추가
+					break;
+				}else {
+					sb.append(deque.pollFirst()).append("\n");//맨 앞 값
+					break;
+				}
+			case 4:
+				if(deque.isEmpty()) {
+					sb.append(-1).append("\n");
+					break;
+				}else {
+					sb.append(deque.pollLast()).append("\n");//맨 뒤 값
+					break;
+				}
+			case 5:
+				sb.append(deque.size()).append("\n");//크기
+				break;
+			case 6:
+				if(deque.isEmpty()) {
+					sb.append(1).append("\n");
+					break;
+				}else {
+					sb.append(0).append("\n");
+					break;
+				}
+			case 7:
+				if(deque.isEmpty()) {
+					sb.append(-1).append("\n");					
+					break;
+				}else {
+					sb.append(deque.peekFirst()).append("\n");//제거 없이 앞에만 뽑아옴
+					break;
+				}
+			case 8:
+				if(deque.isEmpty()) {
+					sb.append(-1).append("\n");
+					break;
+				}else {
+					sb.append(deque.peekLast()).append("\n");//제거 없이 뒤에만뽑아옴
+					break;
+				}
+			}
+		}
+		System.out.print(sb);
+	}
 }
--- WEEK 1/Name/solution2.java
+++ WEEK 1/Name/solution2.java
@@ -1,13 +1,32 @@
-// 문제: 백준 00000번 문제제목
-// 링크: https://www.acmicpc.net/problem/00000
-// 시간: 000ms
+// 문제: 백준 11279 최대 힙
+// 링크: https://www.acmicpc.net/problem/11279
+// 시간: 856ms
 
-public class Main {
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class solution2 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();//시간초과가 있을 수 있으므로
+		int n = sc.nextInt();
 
-        // 이곳에 풀이를 작성합니다.
-        // 다른 사람을 위해 주석을 꼭 달아주세요.
-        // 실패했다면 실패한 이유도 함께 적어주세요.
+		PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());//힙=>PriorityQueue로 생성
+        //collections.reverseOrder=>역순으로 정렬할때 쓰는 매서드
 
-    }
+		for (int i = 0; i < n; i++) {
+			int cmd = sc.nextInt();
+			if (cmd == 0) {
+				if (heap.isEmpty())//비어있으면
+					sb.append(0).append("\n");//sb에 추가
+				else
+					sb.append(heap.poll()).append("\n");//힙에서 나오는 값은 최댓값(reverseorder)
+			} else {
+				heap.add(cmd);
+			}
+		}
+		System.out.println(sb);
+
+	}
 }
