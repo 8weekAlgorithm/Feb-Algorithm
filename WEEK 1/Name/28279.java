9ffc3ec96660e3415671e3e0f4dbcc323b359f53
LeeChangYeol <duf7995@naver.com>
2025-02-02 15:26:15 +0900

    W1 / PART 1 / 이창열

---
+++ WEEK 1/Name/28279.java
@@ -0,0 +1,62 @@
+// 문제: 백준 28279번 덱 2
+// 링크: https://www.acmicpc.net/problem/28279
+// 시간: 1280ms
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // Deque 만들기
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String[] command = br.readLine().split(" ");
+
+            switch (command[0]) {
+                // 맨 앞 X 삽입
+                case "1":
+                    deque.offerFirst(Integer.parseInt(command[1]));
+                    break;
+                // 맨 뒤 X 삽입
+                case "2":
+                    deque.offerLast(Integer.parseInt(command[1]));
+                    break;
+                // 맨 앞 삭제 및 출력
+                case "3":
+                    bw.write(deque.isEmpty() ? "-1\n" : deque.pollFirst() + "\n");
+                    break;
+                // 맨 뒤 삭제 및 출력
+                case "4":
+                    bw.write(deque.isEmpty() ? "-1\n" : deque.pollLast() + "\n");
+                    break;
+                // 정수 개수 출력
+                case "5":
+                    bw.write(deque.size() + "\n");
+                    break;
+                // 비어있는지 확인
+                case "6":
+                    bw.write(deque.isEmpty() ? "1\n" : "0\n");
+                    break;
+                // 맨 앞 출력
+                case "7":
+                    bw.write(deque.isEmpty() ? "-1\n" : deque.peekFirst() + "\n");
+                    break;
+                // 맨 뒤 출력
+                case "8":
+                    bw.write(deque.isEmpty() ? "-1\n" : deque.peekLast() + "\n");
+                    break;
+            }
+        }
+
+        // 출력 후 flush() 및 close()
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
