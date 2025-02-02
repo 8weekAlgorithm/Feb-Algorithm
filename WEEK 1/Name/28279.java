cd177851e236fa3e3b255b5c1996430d79301191
LeeChangYeol <duf7995@naver.com>
2025-02-02 15:45:13 +0900

    W1 / PART 1 / 이창열

--- WEEK 1/Name/28279.java
+++ WEEK 1/Name/28279.java
@@ -1,6 +1,6 @@
 // 문제: 백준 28279번 덱 2
 // 링크: https://www.acmicpc.net/problem/28279
-// 시간: 1280ms
+// 시간: 1332ms
 
 import java.io.*;
 import java.util.*;
@@ -29,11 +29,19 @@ public class Main {
                     break;
                 // 맨 앞 삭제 및 출력
                 case "3":
-                    bw.write(deque.isEmpty() ? "-1\n" : deque.pollFirst() + "\n");
+                	if (deque.isEmpty()) {
+                		bw.write("-1\n");
+                	} else {
+                		bw.write(deque.pollFirst() + "\n");
+                	}
                     break;
                 // 맨 뒤 삭제 및 출력
                 case "4":
-                    bw.write(deque.isEmpty() ? "-1\n" : deque.pollLast() + "\n");
+                    if (deque.isEmpty()) {
+                    	bw.write("-1\n");
+                    } else {
+                    	bw.write(deque.pollLast() + "\n");
+                    }
                     break;
                 // 정수 개수 출력
                 case "5":
@@ -41,15 +49,27 @@ public class Main {
                     break;
                 // 비어있는지 확인
                 case "6":
-                    bw.write(deque.isEmpty() ? "1\n" : "0\n");
+                	if (deque.isEmpty()) {
+                		bw.write("1\n");
+                	} else {
+                		bw.write("0\n");
+                	}
                     break;
                 // 맨 앞 출력
                 case "7":
-                    bw.write(deque.isEmpty() ? "-1\n" : deque.peekFirst() + "\n");
+                	if (deque.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(deque.peekFirst() + "\n");
+                    }
                     break;
                 // 맨 뒤 출력
                 case "8":
-                    bw.write(deque.isEmpty() ? "-1\n" : deque.peekLast() + "\n");
+                	if (deque.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(deque.peekLast() + "\n");
+                    }
                     break;
             }
         }
