be0c9b59cb66dcbdda2751a52e5440eec8a37113
Hyochang098 <wlsgyckd7@naver.com>
2025-02-03 20:26:43 +0900

    제출
    제출

---
+++ WEEK 1/진효창/solution3.java
@@ -0,0 +1,13 @@
+// 문제: 백준 00000번 문제제목
+// 링크: https://www.acmicpc.net/problem/00000
+// 시간: 000ms
+
+public class Main {
+	public static void main(String[] args) {
+
+        // 이곳에 풀이를 작성합니다.
+        // 다른 사람을 위해 주석을 꼭 달아주세요.
+        // 실패했다면 실패한 이유도 함께 적어주세요.
+
+    }
+}
